cc3367f612bf77658b384e4629ff11d822026ed9
RilwanAminu <rilwana61@gmail.com>
2026-05-13 16:22:05 +0100

    added a line

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -8,9 +8,10 @@ const ordersEndpoint = '/orders'
 
 let Incorrect_USERNAME: string
 let Incorrect_PASSWORD: string
-const incorrectLogin = new Login (
+const incorrectLogin = new Login(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  Incorrect_USERNAME='bENNY', Incorrect_PASSWORD='J9087YH098'
+  (Incorrect_USERNAME = 'bENNY'),
+  (Incorrect_PASSWORD = 'J9087YH098'),
 )
 
 test.describe.serial('Authorization flow', () => {
@@ -18,49 +19,47 @@ test.describe.serial('Authorization flow', () => {
     loginDto = new Login(process.env['DL_USERNAME']!, process.env['DL_PASSWORD']!)
   })
 
-
-    test('should login and receive authorization token', async ({ request }) => {
-      const response = await request.post(baseUrl + loginEndpoint, {
-        headers: {
-          'Content-Type': 'application/json',
-          accept: 'application/json',
-        },
-        data: loginDto,
-      })
-
-      expect(response.status()).toBe(200)
-      const token = await response.text()
-      console.log('Received token:', token)
-      expect(token).toBeTruthy()
+  test('should login and receive authorization token', async ({ request }) => {
+    const response = await request.post(baseUrl + loginEndpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      data: loginDto,
     })
 
-    test('should get orders with authorization token', async ({ request }) => {
-      const loginResponse = await request.post(baseUrl + loginEndpoint, {
-        headers: {
-          'Content-Type': 'application/json',
-          accept: 'application/json',
-        },
-        data: loginDto ,
-      })
-
-      expect(loginResponse.status()).toBe(200)
-      const token = await loginResponse.text()
-      expect(token).toBeTruthy()
-
-      const response = await request.get(baseUrl + ordersEndpoint, {
-        headers: {
-          accept: '*/*',
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-      expect(response.status()).toBe(200)
-      const orders = await response.json()
-      console.log('Orders:', JSON.stringify(orders, null, 2))
-      expect(orders).toBeTruthy()
-    })
+    expect(response.status()).toBe(200)
+    const token = await response.text()
+    console.log('Received token:', token)
+    expect(token).toBeTruthy()
   })
 
+  test('should get orders with authorization token', async ({ request }) => {
+    const loginResponse = await request.post(baseUrl + loginEndpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      data: loginDto,
+    })
+
+    expect(loginResponse.status()).toBe(200)
+    const token = await loginResponse.text()
+    expect(token).toBeTruthy()
+
+    const response = await request.get(baseUrl + ordersEndpoint, {
+      headers: {
+        accept: '*/*',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    expect(response.status()).toBe(200)
+    const orders = await response.json()
+    console.log('Orders:', JSON.stringify(orders, null, 2))
+    expect(orders).toBeTruthy()
+  })
+})
 
 test('should not get orders without authorization token', async ({ request }) => {
   const loginResponse = await request.post(baseUrl + loginEndpoint, {
@@ -82,4 +81,3 @@ test('should not get orders without authorization token', async ({ request }) =>
 
   expect(response.status()).toBe(401)
 })
-
